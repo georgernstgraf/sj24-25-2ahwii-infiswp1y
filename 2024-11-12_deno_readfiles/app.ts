d95b4ba7df493e090696c04dfec03e01ff723b8b
Georg Graf (SPG) <grafg@spengergasse.at>
2024-11-12 17:23:23 +0100

    something starts working

--- 2024-11-12_deno_readfiles/app.ts
+++ 2024-11-12_deno_readfiles/app.ts
@@ -1,13 +1,34 @@
-// Define the file path
-const filePath = '../nmap-datenfiles/2024-11-05T13:12:00+01:00';
+
+import { assert } from "https://deno.land/std@0.114.0/testing/asserts.ts";
+
+const dataPath = '../nmap-datenfiles';
+function parseDate(dateStr: string): Date {
+        const p = dateStr.split('_');
+        return new Date(`${p[0]}:${p[1]}:${p[2]}+${p[3]}:${p[4]}`);
+
+}
 try {
-    const dirEntries = await Deno.readDir('../nmap-datenfiles');
+    const dirEntries = await Deno.readDir(dataPath);
     for await (const dirEntry of dirEntries) {
-        console.log(dirEntry.name);
+        if (!dirEntry.isFile) {
+            continue;
+        }
+        let date;
+        try {
+            date = parseDate(dirEntry.name);
+        } catch (err) {
+            assert(err instanceof Error);
+            console.error('Error parsing date:', dirEntry.name, err.message);
+            continue;
+        }
+        // console.log(dirEntry.name, date);
+        const filePath = `${dataPath}/${dirEntry.name}`;
+        const data = await Deno.readTextFile(filePath);
+        const lines = data.split('\n');
+        for (const line of lines) {
+            //console.log(line);
+        }
     }
-    //const data = await Deno.readTextFile(filePath);
-    //const lines = data.split('\n');
-    //console.log(lines);
 } catch (err) {
     console.error('Error reading the file:', err);
 }
